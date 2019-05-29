a0be4d403ae8fa3af6968c9d2ee29a244b4840c8
Dorris Wong <pingki@sbcglobal.net>
2019-05-29 12:02:56 -0700

    editing stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,27 @@
 var Stack = function() {
   var someInstance = {};
-
+  var count = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    count--;
+    var a = storage[count];
+    delete storage[count];
+    return a;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
 };
+
+var s = new Stack();
