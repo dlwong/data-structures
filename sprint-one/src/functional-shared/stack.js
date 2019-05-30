7994aec1da46512b345dd10ef2783eb8c8cf3654
Dorris Wong <pingki@sbcglobal.net>
2019-05-29 22:52:16 -0700

    pass stackMethods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,20 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var instance = {}
-  this.count = 0;
-  this.pop = stackMethods.pop;
-  this.push = stackMethods.push;
-  this.size = Object.keys(instance).length;
+  var instance = {};
+  instance.count = 0;
+  instance.pop = stackMethods.pop;
+  instance.push = stackMethods.push();
+  instance.size = Object.keys(instance).length;
+  instance.storage = {};
 
+  _.extend(instance, stackMethods);
+
+  return instance;
 };
 
 var stackMethods = {
   pop: function(){
-    
+    if(this.size > 0){
+      this.size--;
+      var data = this.storage[this.size];
+      delete this.storage[this.size];
+      return data;
+    }
   },
-  push: function(){
-
+  push: function(value){
+    this.storage[this.size] = data;
+    this.size++;
   }
 };
 
