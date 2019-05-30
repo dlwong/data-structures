c392e486e0bde6efb3b33fc918f450b06ec5a638
Dorris Wong <pingki@sbcglobal.net>
2019-05-29 19:17:57 -0700

    attempt stack functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {}
+  this.count = 0;
+  this.pop = stackMethods.pop;
+  this.push = stackMethods.push;
+  this.size = Object.keys(instance).length;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+  pop: function(){
+    
+  },
+  push: function(){
+
+  }
+};
 
 
